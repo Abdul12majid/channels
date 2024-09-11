f1172d51235f38ed26fa756b27f0ec6d469eb8bd
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-11 08:22:57 +0100

    created Profile model

--- users/models.py
+++ users/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+	profile_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to='img', blank=True, null=True)
+	username = models.CharField(max_length=128, unique=True)
+	info = models.TextField()
+
+	def __str__(self):
+		return f'{self.profile_owner.username}'
