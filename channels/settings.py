5ef21578ba64388dfea1a235e1f27eed8e25ee13
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-12 12:10:46 +0100

    added daphne app

--- channels/settings.py
+++ channels/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -73,7 +74,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "channels.wsgi.application"
+#WSGI_APPLICATION = "channels.wsgi.application"
+ASGI_APPLICATION = 'channels.asgi.application'
 
 
 # Database
