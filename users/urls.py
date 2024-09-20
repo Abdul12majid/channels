6944ec8aa5cc26f34f865a7b6d04c734641c02d5
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-20 07:34:13 +0100

    login view

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("login", views.login_user, name='login-user'),
     path("profile/<str:pk>", views.profile, name='profile'),
+    path("verify_details", views.verify_details, name='verify_details'),
+    path("register", views.register, name='register'),
     
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Profile
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password1']
+		check_username = User.objects.filter(username=username)
+		if check_username.exists():
+			user = authenticate(request, username=username, password=password)
+			if User is not None:
+				login(request, user)
+				return redirect('index')
+			else:
+				print("error loggin in")
+				return redirect('index')
+		return redirect('register')
+
 	return render(request, 'login.html')
 
 
@@ -16,3 +31,15 @@ def profile(request, pk):
 		"profile":profile,
 	}
 	return render(request, 'profile.html', context)
+
+
+def verify_details(request):
+	username = request.post.get('username')
+	if User.objects.filter(username=username).exists():
+		return HttpResponse("<p>You can login</p>")
+	else:
+		return HttpResponse("<p>Username not found</p>")
+
+def register(request):
+	return HttpResponse("register p[age]")
+
