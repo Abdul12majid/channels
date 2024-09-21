0f2d206553704451077053217789ad9f6562f9d4
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-21 09:55:47 +0100

    verify password htmx

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("profile/<str:pk>", views.profile, name='profile'),
     path("verify_log_details", views.verify_log_details, name='verify_log_details'),
     path("verify_username", views.verify_username, name='verify_username'),
+    path("verify_password", views.verify_password, name='verify_password'),
     path("register", views.register, name='register'),
     
     
